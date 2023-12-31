b3c0b05f80fce82a8af780a12555c574f737017c
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-01 00:58:08 +0200

    main was created

---
+++ main.py
@@ -0,0 +1,78 @@
+import json
+
+import scrapy
+from itemadapter import ItemAdapter
+from scrapy.crawler import CrawlerProcess
+from scrapy.item import Item, Field
+
+from db_seed import seed_authors, seed_quotes
+
+AUTHORS_FILE = "authors.json"
+QUOTES_FILE = "quotes.json"
+
+
+class QuoteItem(Item):
+    quote = Field()
+    author = Field()
+    tags = Field()
+
+
+class AuthorItem(Item):
+    fullname = Field()
+    born_date = Field()
+    born_location = Field()
+    description = Field()
+
+
+class DataPipline:
+    quotes = []
+    authors = []
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if 'fullname' in adapter.keys():
+            self.authors.append(dict(adapter))
+        if 'quote' in adapter.keys():
+            self.quotes.append(dict(adapter))
+
+    def close_spider(self, spider):
+        with open(AUTHORS_FILE, 'w', encoding='utf-8') as fd:
+            json.dump(self.authors, fd, ensure_ascii=False, indent=2)
+        with open(QUOTES_FILE, 'w', encoding='utf-8') as fd:
+            json.dump(self.quotes, fd, ensure_ascii=False, indent=2)
+        seed_authors(AUTHORS_FILE)
+        seed_quotes(QUOTES_FILE)
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "get_quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+    custom_settings = {"ITEM_PIPELINES": {DataPipline: 300}}
+
+    def parse(self, response, **kwargs):
+        for q in response.xpath("/html//div[@class='quote']"):
+            quote = q.xpath("span[@class='text']/text()").get().strip()
+            author = q.xpath("span/small[@class='author']/text()").get().strip().replace('-', ' ')
+            tags = q.xpath("div[@class='tags']/a/text()").extract()
+            yield QuoteItem(quote=quote, author=author, tags=tags)
+            yield response.follow(url=self.start_urls[0] + q.xpath("span/a/@href").get(), callback=self.parse_author)
+
+        next_link = response.xpath("/html//li[@class='next']/a/@href").get()
+        if next_link:
+            yield scrapy.Request(url=self.start_urls[0] + next_link)
+
+    @classmethod
+    def parse_author(cls, response, **kwargs):
+        content = response.xpath("/html//div[@class='author-details']")
+        fullname = content.xpath("h3[@class='author-title']/text()").get().strip().replace('-', ' ')
+        born_date = content.xpath("p/span[@class='author-born-date']/text()").get().strip()
+        born_location = content.xpath("p/span[@class='author-born-location']/text()").get().strip()
+        description = content.xpath("div[@class='author-description']/text()").get().strip()
+        yield AuthorItem(fullname=fullname, born_date=born_date, born_location=born_location, description=description)
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(QuotesSpider)
+    process.start()
